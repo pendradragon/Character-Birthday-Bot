9732dd315ac7773c015f498777e59a1f4044b361
Megan <megante.kerstetter@gmail.com>
2024-06-29 22:06:42 -0400

    Changed 'add' command for time complexity reason

--- commandConfig/dictCommands.py
+++ commandConfig/dictCommands.py
@@ -8,11 +8,10 @@ class commands:
     #add command
     @base.bot.command(name='add')
     async def add(ctx, arg1:str = commands.parameter(defualt = 'Character name', description = "Your character's name goes here"), arg2:str = commands.parameter(default = "Birthday", description = "Insert the date of birth in the format <month (spelt) day (number)>")):
-        for entry in serverStuff.serverDict:
-            #runs if the character is ALREADY in the dictionary
-            if entry.key == arg1:
-                print(arg1 + " is already in the birthday logs!")
-            
-            #runs if the character is NOT in the dictionary
-            else:
-                serverStuff.serverDict[arg1]=arg2
+        #runs if the character is in the dictionary alread
+        if arg1 in serverStuff.serverDict:
+            print(arg1 + " is already in the birth logs!")
+        
+        #runs if the character is not in the dictionary -- should add it
+        else:
+            serverStuff.serverDict[arg1] = arg2
