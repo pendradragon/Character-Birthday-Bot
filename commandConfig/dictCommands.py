7bdd94b2aa216e806292d9252381381a7812a160
Megan <megante.kerstetter@gmail.com>
2024-07-02 23:54:07 -0400

    Started the calenday portion and checking part of the code

--- commandConfig/dictCommands.py
+++ commandConfig/dictCommands.py
@@ -1,6 +1,10 @@
 from main import base
+from datetime import datetime
 
 class dictBasics:
+    #used in the actual calendar portion of the code
+    calendarDict = dict()
+
     #add function
     def add(CharacterName, DOB):
         #if the character is in the list of characters
@@ -8,6 +12,7 @@ class dictBasics:
             print(CharacterName + " is already in the the birthday logs.")
         else:
             base.serverDict[CharacterName] = DOB
+            dictBasics.calendarDict[CharacterName] = datetime.strptime(DOB, '%m-%d').date()
             print(CharacterName + " was added to the birthday logs.")
 
     #remove function
@@ -16,6 +21,7 @@ class dictBasics:
             print(CharacterName + " is not in the birthday logs.")
         else:
             base.serverDict.pop(CharacterName)
+            dictBasics.calendarDict.pop(CharacterName)
             print(CharacterName + " was removed from the birthday logs")
 
     #find DOB function
