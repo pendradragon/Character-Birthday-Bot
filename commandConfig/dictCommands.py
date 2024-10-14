1ab118e0343de7eb7c456b6c34ea0078bd9d1492
Megan <megante.kerstetter@gmail.com>
2024-10-13 21:00:07 -0400

    Changed the dictionary commands so they are more compact and less all over the place

--- commandConfig/dictCommands.py
+++ commandConfig/dictCommands.py
@@ -1,32 +1,11 @@
-from main import base
-from datetime import datetime
+from birthdayData import birthdays  
 
-class dictBasics:
-    #used in the actual calendar portion of the code
-    calendarDict = dict()
+#command to add characters to the global dictionary
+def add(name, date):
+    birthdays[name] = date
 
-    #add function
-    def add(CharacterName, DOB):
-        #if the character is in the list of characters
-        if CharacterName in base.serverDict:
-            print(CharacterName + " is already in the the birthday logs.")
-        else:
-            base.serverDict[CharacterName] = DOB
-            dictBasics.calendarDict[CharacterName] = datetime.strptime(DOB, '%m-%d').date()
-            print(CharacterName + " was added to the birthday logs.")
-
-    #remove function
-    def remove(CharacterName):
-        if CharacterName not in base.serverDict:
-            print(CharacterName + " is not in the birthday logs.")
-        else:
-            base.serverDict.pop(CharacterName)
-            dictBasics.calendarDict.pop(CharacterName)
-            print(CharacterName + " was removed from the birthday logs")
-
-    #find DOB function
-    def findDOB(CharacterName):
-        if CharacterName not in base.serverDict:
-            print(CharacterName + " could not be found in the birthday logs")
-        else:
-            print(CharacterName + " was born on " + base.serverDict[CharacterName])
+#command to remove the desired command from the global dictionary
+def remove(name):
+    if name in birthdays:
+        del birthdays[name]
+        
