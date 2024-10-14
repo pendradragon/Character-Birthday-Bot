21a492272e04888af48fbf41f46561f97c1f6f20
Megan <megante.kerstetter@gmail.com>
2024-10-13 21:12:32 -0400

    Making it so the user can define what they want the message to be

--- commandConfig/checker.py
+++ commandConfig/checker.py
@@ -3,6 +3,9 @@ from datetime import datetime
 from birthdayData import birthdays
 from birthdayData import message
 
+from configs import getChannel
+
 async def check_birthdays(bot):
     # checks if today's date matches any birthday in the directory
-
+    today = datetime.now().strftime('%m-%d')
+    
