5ec9f5284dc72041e510879b6c1b70eb6a3f59e1
Megan <megante.kerstetter@gmail.com>
2024-10-13 21:22:51 -0400

    Changing the bot bc I made it more channeling than it needed to be

--- main.py
+++ main.py
@@ -1,54 +1,26 @@
 import os
+from datetime import datetime
 
+#discord imports
 import discord
+from discord.ext import commands, tasks
+
+#Security is wild
 from dotenv import load_dotenv
 
-from discord.ext import commands
+#Custom commands importing
+from dictCommands import add, remove, getDOB, get_character_by_DOB
+from checker import check_birthdays
 
-#commands code imports
-from dictCommands import dictBasics
+#importing data be like
+from birthdayData import birthdays, setMessage, getMessage
+from configs import setChannel, getChannel
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 #intents creation -- set to default for now until I figure out what they actually do
 intents = discord.Intents.default()
+intents.message_content = True
 
-#used universally throughout the bot
-class base:
-    client = discord.Client(intents=intents)
-    guild = discord.guild.__name__
-    bot = commands.Bot(command_prefix="c!")
-    serverDict = dict()
-
-    bDayMessage = str()
-
-#basic setup commands
-@base.bot.command(name="setMessage")
-async def setMessage(ctx, arg1:str = commands.parameter(default = "The message that is sent every time it is a character's birthday.", description = None)):
-    base.bDayMessage = arg1
-
-@base.bot.command(name = "help")
-async def help(ctx):
-    print("# Character Birthday Bot Help #" + "\n" +
-          "setMessage -- sets the message that is sent every time it is a character's birthday" + "\n"+
-          "add -- adds a character to the bot's birthday logs" + "\n" +
-          "remove -- removes a character from the bot's birthday logs" + "\n" +
-          "findDOB -- finds the character with the entered date of birth")
-    
-#bot commands from the dictCommands file
-@base.bot.command(name = "add")
-async def add(ctx, arg1:str = commands.parameter(default="Character name", description="Insert your characters name"), arg2:str = commands.parameter(default = "Your character's date of birth", description = "Insert your character's date of birth in the format MM-DD")):
-    dictBasics.add(arg1, arg2)
-
-@base.bot.command(name = "remove")
-async def remove(ctx, arg1:str = commands.parameter(default= "Character name", description="The name of the character you want to remove from the birthday logs")):
-    dictBasics.remove(arg1)
-
-@base.bot.command(name="findDOB")
-async def findDOB(ctx, arg1:str = commands.parameter(default = "Character name", description="Finds the date of the birth of the provided character")):
-    dictBasics.findDOB(arg1)
-
-
-#runs the bot
-base.client.run(TOKEN)
+bot = commands.Bot(command_prefix='!', intents = intents)
