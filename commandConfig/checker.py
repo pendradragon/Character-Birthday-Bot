cd0a5897f1bf8132e4c81762fdda6105c4db31e2
Megan <megante.kerstetter@gmail.com>
2024-10-13 21:13:51 -0400

    Changing the checker so that the logic is more sound

--- commandConfig/checker.py
+++ commandConfig/checker.py
@@ -8,4 +8,14 @@ from configs import getChannel
 async def check_birthdays(bot):
     # checks if today's date matches any birthday in the directory
     today = datetime.now().strftime('%m-%d')
-    
+    sendChannel = getChannel()
+
+    if sendChannel: #if there is a channel ID saved there
+        channel = bot.get_channel(sendChannel)
+
+        if channel:
+            for name, date in birthdays.items():
+                if date == today:
+                    #format the message using the template that the user picked out
+                    sentMessage = message.format(name=name)
+                    await channel.send(message)
